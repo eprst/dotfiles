41e281500afc785be176b70402aaf4690f94c304
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2013-07-24 16:01:28 -0700

    fixed update script

--- script/update.sh
+++ script/update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
-echo -e "  \e[32mrunning git pull\e[39m"
+echo -e "  \033[32mrunning git pull\033[39m"
 git pull
-echo -e "  \e[32mrunning git submodule update --recursive\e[39m"
-git submodule update --recursive
-echo -e "  \e[32mrunning git submodule foreach git checkout master\e[39m"
+echo -e "  \033[32mrunning git submodule foreach git checkout master\033[39m"
 git submodule foreach git checkout master
+echo -e "  \033[32mrunning git submodule foreach git pull\033[39m"
+git submodule foreach git pull
