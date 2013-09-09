a836c1e7914fa1488aed969bc9fbb3301d4ca61a
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2013-09-09 14:27:38 -0700

    made /usr/local/bin to go first in the PATH (for brew)

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,3 @@
 [[ -d /opt/local/bin ]] && export PATH=$PATH:/opt/local/bin
+[[ -d /usr/local/bin ]] && export PATH=/usr/local/bin:$PATH
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
