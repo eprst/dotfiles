db8a52b941859426a6bbc0842e66f75cac6df9e7
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2013-10-14 22:44:12 -0700

    zsh option changes

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -19,6 +19,7 @@ setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
+setopt NO_FLOW_CONTROL
 setopt COMPLETE_IN_WORD
 # setopt IGNORE_EOF
 
@@ -26,7 +27,10 @@ setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
+setopt HIST_ALLOW_CLOBBER
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
+
+cdpath=( ~/workspace )
