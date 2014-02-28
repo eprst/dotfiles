63dca9c27a58ebd4c9a1bd4d4fb30df082041d2c
Konstantin Sobolev <ksobolev@linkedin.com>
2014-02-28 14:19:05 -0800

    disabled history sharing in zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -15,7 +15,6 @@ setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
@@ -23,8 +22,9 @@ setopt NO_FLOW_CONTROL
 setopt COMPLETE_IN_WORD
 # setopt IGNORE_EOF
 
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+# setopt APPEND_HISTORY # adds history
+# setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt INC_APPEND_HISTORY # incrementally appends history but doesn't share it. Use fc -RI to import
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 setopt HIST_ALLOW_CLOBBER
