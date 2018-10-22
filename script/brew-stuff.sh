36e7e288d5dbce06675f1cf52b420e8d139030ab
Konstantin Sobolev <konstantin@sumologic.com>
2018-10-22 16:30:52 -0700

    misc updates

--- script/brew-stuff.sh
+++ script/brew-stuff.sh
@@ -1,3 +1,4 @@
 brew install reattach-to-user-namespace --wrap-pbcopy-and-pbpaste
 brew install mc vim wget zsh tmux aria2 ag gcal tig htop w-calc ctags dialog
 brew cask install macvim --override-system-vim
+brew cask install speedcrunch
--- zsh/history.zsh
+++ zsh/history.zsh
@@ -2,7 +2,7 @@ export HISTSIZE=1000
 export SAVEHIST=1000
 export HISTFILE=$HOME/.zsh_history
 # setopt inc_append_history
-# setopt share_history
+unsetopt share_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups
 setopt hist_find_no_dups
