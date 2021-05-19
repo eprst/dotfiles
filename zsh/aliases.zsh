90bdfe2cf46fc89c64c779d8054266531ab15dc7
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2021-05-19 14:20:47 -0700

    added video alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -24,7 +24,8 @@ fi
 
 alias v='vim'
 alias nv='nvim'
-alias m='mutt'
-alias oi='offlineimap'
+# alias m='mutt'
+# alias oi='offlineimap'
 alias socks='ssh -D 8080 -q -C -N -p 2212 kos@sobolev.io'
 alias psg='ps auxw | grep'
+alias video='gphoto2 --set-config=/main/actions/autofocus=1 && gphoto2 --stdout --capture-movie | ffmpeg -hwaccel nvdec -c:v mjpeg_cuvid -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0'
