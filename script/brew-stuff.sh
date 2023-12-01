9f50ab13a97e579cd23333ff11afd6c46b581f68
eprst <konstantin.sobolev@gmail.com>
2023-11-30 22:07:57 -0800

    brew updates

--- script/brew-stuff.sh
+++ script/brew-stuff.sh
@@ -1,15 +1,18 @@
-brew install reattach-to-user-namespace --wrap-pbcopy-and-pbpaste
-brew install mc vim wget zsh tmux aria2 ag gcal tig htop w-calc ctags dialog
-brew cask install macvim --override-system-vim
-brew cask install speedcrunch
+# brew install reattach-to-user-namespace --wrap-pbcopy-and-pbpaste
+# brew install mc vim wget zsh tmux aria2 ag gcal tig htop w-calc ctags dialog
+# brew cask install macvim --override-system-vim
+brew install gawk fzf bat fish ncspot jq gcal ripgrep
+brew install --cask speedcrunch iterm2
 
 # nerd fonts
-brew tap homebrew/cask-fonts
-brew cask install font-hack-nerd-font
-brew cask install font-firacode-nerd-font-mono
-brew cask install font-firacode-nerd-font
+# brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+# brew cask install font-firacode-nerd-font-mono
+# brew cask install font-firacode-nerd-font
 
 # neovim
-brew install https://raw.githubusercontent.com/equalsraf/homebrew-neovim-qt/master/neovim-qt.rb
-brew install python3
-pip3 install neovim --upgrade
+# brew install https://raw.githubusercontent.com/equalsraf/homebrew-neovim-qt/master/neovim-qt.rb
+# brew install python3
+# pip3 install neovim --upgrade
+brew install neovim
+brew install --cask fvim
